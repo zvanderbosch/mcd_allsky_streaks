32eb3819f2116339769be57e4121fd31a35cc333
Zach Vanderbosch <zachary.vanderbosch@austin.utexas.edu>
2026-09-24 15:27:17 -0500

    Add image processing script

---
+++ process_image.py
@@ -0,0 +1,26 @@
+"""All-sky image processing routines
+
+This script will process and analyze all-sky fisheye images
+to detect satellite streaks.
+
+Usage:
+    TBD
+
+Arguments:
+    TBD
+
+Options:
+    TBD
+
+Input Files Required:
+    TBD
+
+Author:
+    Z. Vanderbosch (HET)
+
+Last Updated:
+    2026 Sept 24
+
+"""
+
+
