86b063e1e6183b2eb35f93fc79211fda7d3df126
Zach Vanderbosch <zachary.vanderbosch@austin.utexas.edu>
2026-09-25 20:50:33 -0500

    Add basic plate solving capabilities

--- src/process_image.py
+++ src/process_image.py
@@ -4,33 +4,25 @@ This script will process and analyze all-sky fisheye images
 to detect streaks from satellites and potentially from other 
 artificial and natural sources (e.g. planes, meteors, NEOs).
 
-Usage:
-    TBD
-
-Arguments:
-    TBD
-
-Options:
-    TBD
-
-Input Files Required:
-    TBD
-
 Author:
     Z. Vanderbosch (HET)
 
 Last Updated:
-    2026 Sept 24
+    2026 Sept 25
 
 """
 
 import os
+import subprocess
 import numpy as np
 
 from pathlib import Path
 from dataclasses import dataclass
 from typing import Any, Mapping
 from astropy.io import fits
+from astropy.table import Table
+from astropy.stats import sigma_clipped_stats
+from photutils.detection import DAOStarFinder
 
 
 @dataclass(frozen=True)
@@ -38,7 +30,7 @@ class ImageData:
     """Class to store all-sky image data, header, and path"""
 
     data: np.ndarray
-    header: Mapping[str, Any]
+    header: Mapping[str, Any] | None
     path: str | None
 
 
@@ -73,7 +65,7 @@ class ImageIO:
     @staticmethod
     def create(
         data: np.ndarray,
-        header: Mapping[str, Any],
+        header: Mapping[str, Any] | None = None,
         save_to: str | Path | None = None
     ) -> ImageData:
         """
@@ -83,9 +75,12 @@ class ImageIO:
         
         # Save to file if a path is given
         if save_to:
-            if isinstance(header,dict):
-                header = fits.Header(header)
-            newHDU = fits.PrimaryHDU(data=data, header=header)
+            if header:
+                if isinstance(header,dict):
+                    header = fits.Header(header)
+                newHDU = fits.PrimaryHDU(data=data, header=header)
+            else:
+                newHDU = fits.PrimaryHDU(data=data)
             newHDU.writeto(save_to, overwrite=True)
             print(f'Saved to FITS file: {save_to}')
         
@@ -94,3 +89,103 @@ class ImageIO:
             header=header,
             path=save_to
         )
+    
+
+
+class PlateSolve:
+    """Utilities for plate solving all-sky images"""
+
+    @staticmethod
+    def findStars(image: ImageData) -> str | Path:
+        """
+        Function to detect sources in an image and save XY positions
+        to an output file in same directory as the image
+
+        Parameters:
+        -----------
+        image: ImageData object
+            Object containing image data, header, and source file name
+
+        Returns:
+        --------
+        sourceFileXY: str or Path
+            FITS table filename where XY positions are saved
+        """
+
+        # Get some image stats
+        _,_,std = sigma_clipped_stats(
+            image.data, sigma=3.0
+        )
+
+        # Find stars in image
+        daoFinder = DAOStarFinder(
+            threshold=4.0*std, 
+            fwhm=3.0,
+            peak_max=50000,
+            n_brightest=100
+        )
+        daoSources = daoFinder(image.data)
+
+        # Save sources to file
+        sourceFileXY = f"{image.path.split('.')[0]}_xy.fits"
+        sourceTable = Table(
+            {
+                'x':daoSources['x_centroid'],
+                'y':daoSources['y_centroid']
+            }
+        )
+        sourceTable.write(sourceFileXY, format='fits', overwrite=True)
+
+        return sourceFileXY
+
+    @staticmethod
+    def solveWithANetOnline(
+        xyFile: str | Path,
+        xsize: int,
+        ysize: int
+    ):
+        """
+        Function that sends a list of XY pixel coordinates for
+        detected objects, along with the image X and Y extentz,
+        to the Astrometry.net online service to obtain the image
+        plate solution as WCS header keyword values.
+
+        Parameters:
+        -----------
+        xyFile: str
+            FITS table filename containing source XY positions
+        xsize: int
+            Image width (pixels)
+        ysize: int
+            Image height (pixels)
+
+        Returns:
+        --------
+        response.returncode: int
+            Return code from subprocess
+        """
+        
+        astDir = "/".join(xyFile.split("/")[0:-1])
+        fileBase = xyFile.split("/")[-1].split(".")[0]
+
+        # Plate solve using Astrometry.net client (anet_client.py)
+        cmd = [
+            'python', '/home/zach/HET/mcd_allsky_streaks/src/anet_client.py', 
+            '--apikey', 'kdvqtjbqkbkbuyzb',
+            '--upload-xy', f'{xyFile}',
+            '--parity', '2',
+            '--image-width', f'{xsize}',
+            '--image-height', f'{ysize}',
+            '--scale-units', 'arcsecperpix',
+            '--scale-est', '70.0',
+            '--scale-err', '10.0', # percent
+            '--calibrate', f'{astDir}/{fileBase}_calib.txt',
+            '--wcs', f'{astDir}/{fileBase}_wcs.fits',
+            '--solve-time', '30.0'
+        ]
+        response = subprocess.run(cmd, timeout=None)
+        if response.check_returncode():
+            print(f'ERROR: Return code {response.returncode} from Astrometry.net API')
+            
+        return response.returncode
+
--- testrun.py
+++ testrun.py
@@ -1,11 +1,21 @@
 """Script used to test-run the image processing routines"""
 
-# Add src directory to path
 import sys
+
 sys.path.insert(0, "./src")
 
-from process_image import ImageIO
+import subprocess
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.colors as mcolors
+
+from glob import glob
 from astropy.visualization import PercentileInterval
+
+from process_image import ImageIO
+from process_image import PlateSolve
+
 
 # Define some base directories
 BASE = '/home/zach/HET'
@@ -16,6 +26,7 @@ DIRS = {
     'conf':f'{BASE}/mcd_allsky_streaks/config'
 }
 
+
 # Load the raw FITS images
 fitsFile1 = f"{DIRS['data']}/ALPACA.2026-08-08T23:22:17.570.fits"
 fitsFile2 = f"{DIRS['data']}/ALPACA.2026-08-08T23:24:21.850.fits"
@@ -23,10 +34,45 @@ image1 = ImageIO.load(fitsFile1)
 image2 = ImageIO.load(fitsFile2)
 
 
+# Divide the original images into a grid of smaller images
+cs = 256  # crop size for stamps in pixel units
+numrings = 1
+ydim,xdim = image1.data.shape
+xcen = np.floor(xdim/2)+1
+ycen = np.floor(ydim/2)+1
+
+for ring in range(numrings):
+    for xshift in range(-ring,ring+1):
+        for yshift in range(-ring,ring+1):
+
+            # Make sure the shift belongs to the given ring
+            if (abs(xshift) < ring) and (abs(yshift) < ring):
+                continue
+
+            # Create new ImageData object for stamp
+            stampPath = f"{DIRS['data']}/stamps/stamp_r{ring:02d}_{xshift:+03d}{yshift:+03d}.fits"
+            stampPath = stampPath.replace("-","m").replace("+","p")
+            stampXcen = xcen + xshift*cs
+            stampYcen = ycen + yshift*cs
+            stampImage = ImageIO.create(
+                data = image2.data[
+                    int(stampYcen-cs/2):int(stampYcen+cs/2),
+                    int(stampXcen-cs/2):int(stampXcen+cs/2)
+                ],
+                save_to=stampPath
+            )
+
+            # Get source XY positions
+            sourceFileXY = PlateSolve.findStars(stampImage)
+
+            # Plate solve using source XY positions
+            responseCode = PlateSolve.solveWithANetOnline(sourceFileXY,cs,cs)
+
+
 # Take image difference and create new ImageData object
-diffimPath = f"{DIRS['data']}/diffim.fits"
-diffimage = ImageIO.create(
-    data=image1.data-image2.data,
-    header=image2.header,
-    save_to=diffimPath
-)
+# diffimPath = f"{DIRS['data']}/diffim.fits"
+# diffimage = ImageIO.create(
+#     data=image1.data-image2.data,
+#     header=image2.header,
+#     save_to=diffimPath
+# )
